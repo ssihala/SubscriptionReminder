a7dc2cf881f8a08dd190e3ae31f7935dc9b4f761
sihala <ssihala@gmail.com>
2022-09-10 20:58:56 -0400

    Add input submission function

--- src/components/AddSubscription.js
+++ src/components/AddSubscription.js
@@ -4,6 +4,15 @@ import { Button, Modal, Form } from 'react-bootstrap';
 function AddButton() {
   const [show, setShow] = useState(false);
 
+  const formSubmit = (event) => {
+    event.preventDefault();
+
+    const subName = event.target.serviceName;
+    const subPrice = event.target.servicePrice;
+    const subDate = event.target.serviceStart;
+
+  }
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -17,29 +26,27 @@ function AddButton() {
           <Modal.Title>New Subscription</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Form>
-            <Form.Group className="name" controlId="ServiceName">
+            <Form onSubmit={formSubmit}>
+            <Form.Group className="name"  controlId="ServiceName">
                 <Form.Label>Service Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter name"/>
+                <Form.Control type="text" name="serviceName" placeholder="Enter name"/>
             </Form.Group>
             <Form.Group className="price" controlId="SubPrice">
                 <Form.Label>Subscription Price</Form.Label>
-                <Form.Control type="text" placeholder="Price"/>
+                <Form.Control type="text" name="servicePrice" placeholder="Price"/>
             </Form.Group>
             <Form.Group className="date" controlId="SubDate">
-                <Form.Label>Expiration Date</Form.Label>
-                <Form.Control type="date" placeholder="Date"/>
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control type="date" name="serviceStart" placeholder="Date"/>
             </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" type="submit" onClick={handleClose}>
             Save Changes
           </Button>
-        </Modal.Footer>
+            </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
